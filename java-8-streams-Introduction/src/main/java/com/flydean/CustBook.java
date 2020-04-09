41b6a20003af25a257a1154c771cf1b132f05bbc
wayne <wayne.fu@jfz.com>
2020-04-09 22:02:41 +0800

    java 8 streams introduction

---
+++ java-8-streams-Introduction/src/main/java/com/flydean/CustBook.java
@@ -0,0 +1,15 @@
+package com.flydean;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author wayne
+ * @version CustBook,  2020/4/9 9:59 下午
+ */
+@Data
+public class CustBook {
+
+    List<String> bookName;
+}
---
+++ java-8-streams-Introduction/src/main/java/com/flydean/StreamUsage.java
@@ -0,0 +1,69 @@
+package com.flydean;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author wayne
+ * @version StreamUsage,  2020/4/9 9:24 下午
+ */
+public class StreamUsage {
+
+    public static void main(String[] args) {
+
+        //Stream Creation
+        String[] arr = new String[]{"a", "b", "c"};
+        Stream<String> stream = Arrays.stream(arr);
+        stream = Stream.of("a", "b", "c");
+
+        //Multi-threading
+        List<String> list =new ArrayList();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("abc");
+        list.add("ccc");
+        list.add("ddd");
+        Stream<String> listStream = list.stream();
+        list.parallelStream().forEach(element -> doPrint(element));
+
+        //Operations
+        long count = list.stream().distinct().count();
+
+        //Matching
+        boolean isValid = list.stream().anyMatch(element -> element.contains("h")); // true
+        boolean isValidOne = list.stream().allMatch(element -> element.contains("h")); // false
+        boolean isValidTwo = list.stream().noneMatch(element -> element.contains("h")); // false
+
+        //Filtering
+        Stream<String> filterStream = list.stream().filter(element -> element.contains("d"));
+
+        //Mapping
+        Stream<String> mappingStream = list.stream().map(element -> convertElement(element));
+
+        //FlatMap
+        List<CustBook> users = new ArrayList<>();
+        users.add(new CustBook());
+        Stream<String> userStream
+                = users.stream().flatMap(user -> user.getBookName().stream());
+
+        //Reduction
+        List<Integer> integers = Arrays.asList(1, 1, 1);
+        Integer reduced = integers.stream().reduce(100, (a, b) -> a + b);
+
+        //Collecting
+        List<String> resultList
+                = list.stream().map(element -> element.toUpperCase()).collect(Collectors.toList());
+
+    }
+
+    private static String convertElement(String element) {
+        return "element"+"abc";
+    }
+
+    private static void doPrint(String element) {
+        System.out.println(element);
+    }
+}
